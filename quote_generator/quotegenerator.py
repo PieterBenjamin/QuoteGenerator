79ed958cd76c158bc87aeceb6f7aca84dfdf1a1d
PieterBenjamin <pieter0benjamin@gmail.com>
2019-03-07 13:53:40 -0800

    first commit

---
+++ quote_generator/quotegenerator.py
@@ -0,0 +1,12 @@
+import os
+import json
+import random
+from pprint import pprint
+
+json_data=open(str(os.path.expanduser('~/gitrepos/QuoteGenerator/quote_generator/tolkienquotesjson.txt'))).read()
+
+data = json.loads(json_data)
+
+quoteIndex = random.randint(0, len(data) + 1)
+print(data[quoteIndex]['quote'])
+print("-" + data[quoteIndex]['author'] + ", " + data[quoteIndex]['source'])
