075ddfa1ae151d5c05a4ef6e3bdbc289760eab3a
PieterBenjamin <pieter0benjamin@gmail.com>
2019-04-04 09:29:55 -0700

    added word wrapping

--- quote_generator/quotegenerator.py
+++ quote_generator/quotegenerator.py
@@ -1,15 +1,21 @@
 import os
 import json
 import random
-from pprint import pprint
+import textwrap
 
-json_data=open(str(os.path.expanduser('~/gitrepos/QuoteGenerator/quote_generator/tolkienquotesjson.txt'))).read()
+path_to_file = '~/gitrepos/QuoteGenerator/quote_generator/tolkienquotesjson.txt'
+json_data=open(str(os.path.expanduser(path_to_file))).read()
 
 data = json.loads(json_data)
 
 quoteIndex = random.randint(0, len(data) - 1)
 print()
-print(data[quoteIndex]['quote'])
+quote = data[quoteIndex]['quote']
+wrapped_quote = textwrap.wrap(quote[1 : len(quote) - 1], 80)
+
+for quoteSegment in wrapped_quote:
+    print(quoteSegment)
+
 print()
 print("\t-" + data[quoteIndex]['author'] + ", " + data[quoteIndex]['source'])
 print()
