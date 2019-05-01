7b09cde5eec80c541d9e90ae58a8ffad7c0c8144
PieterBenjamin <pieter0benjamin@gmail.com>
2019-05-01 09:36:33 -0700

    made variables easier to edit

--- quote_generator/quotegenerator.py
+++ quote_generator/quotegenerator.py
@@ -3,19 +3,25 @@ import json
 import random
 import textwrap
 
-path_to_file = '~/gitrepos/QuoteGenerator/quote_generator/tolkienquotesjson.txt'
-json_data=open(str(os.path.expanduser(path_to_file))).read()
+# Put the absolute (or relative) pathway to the json quote file.
+path_to_json_file = '~/gitrepos/QuoteGenerator/quote_generator/tolkienquotesjson.txt'
+# Python handles opening up the json
+json_data   = open(str(os.path.expanduser(path_to_json_file))).read()
+quote_data  = json.loads(json_data)
+# Variable which specifies how many characters to include in
+# the quote before wrapping.
+quote_width = 80
 
-data = json.loads(json_data)
-
-quoteIndex = random.randint(0, len(data) - 1)
+# Random selection
+quote_index = random.randint(0, len(quote_data) - 1)
 print("")
-quote = data[quoteIndex]['quote']
-wrapped_quote = textwrap.wrap(quote[1 : len(quote) - 1], 80)
+quote = quote_data[quote_index]['quote']
+wrapped_quote = textwrap.wrap(quote[1 : len(quote) - 1], quote_width)
 
+# Printing the actual quote.
 for quoteSegment in wrapped_quote:
     print(quoteSegment)
-
+# Printing the author/source.
 print("")
-print("\t-" + data[quoteIndex]['author'] + ", " + data[quoteIndex]['source'])
+print("\t-" + quote_data[quote_index]['author'] + ", " + quote_data[quote_index]['source'])
 print("")
