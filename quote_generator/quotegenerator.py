2c4309c37cbd4436e151a71c373d2e8fa971eab0
PieterBenjamin <pieter0benjamin@gmail.com>
2019-05-06 17:44:26 -0700

    changed to quote scraping style

--- quote_generator/quotegenerator.py
+++ quote_generator/quotegenerator.py
@@ -1,10 +1,11 @@
 import os
+import sys
 import json
 import random
 import textwrap
 
 # Put the absolute (or relative) pathway to the json quote file.
-path_to_json_file = '~/gitrepos/QuoteGenerator/quote_generator/tolkienquotesjson.txt'
+path_to_json_file = '~/gitrepos/QuoteGenerator/quote_generator/TolkienQuotesJson/TolkienQuotes-%s.json' % str(random.randint(0, int(sys.argv[1])))
 # Python handles opening up the json
 json_data   = open(str(os.path.expanduser(path_to_json_file))).read()
 quote_data  = json.loads(json_data)
@@ -15,13 +16,11 @@ quote_width = 80
 # Random selection
 quote_index = random.randint(0, len(quote_data) - 1)
 print("")
-quote = quote_data[quote_index]['quote']
+quote = quote_data[quote_index]
 wrapped_quote = textwrap.wrap(quote[1 : len(quote) - 1], quote_width)
 
 # Printing the actual quote.
 for quoteSegment in wrapped_quote:
     print(quoteSegment)
 # Printing the author/source.
-print("")
-print("\t-" + quote_data[quote_index]['author'] + ", " + quote_data[quote_index]['source'])
 print("")
